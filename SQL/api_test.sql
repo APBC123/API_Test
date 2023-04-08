5577ef04ba7739d33b1cb55b4c0955126428daf5
“APBC123” <2290312980@qq.com>
2023-04-08 10:01:39 +0800

    master

--- SQL/api_test.sql
+++ SQL/api_test.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80028
  File Encoding         : 65001
 
- Date: 07/04/2023 19:32:31
+ Date: 08/04/2023 10:00:02
 */
 
 SET NAMES utf8mb4;
@@ -22,28 +22,31 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `keywords`;
 CREATE TABLE `keywords`  (
-  `id` int NOT NULL AUTO_INCREMENT,
   `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `pinyin` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `content`(`content` ASC) USING BTREE,
+  PRIMARY KEY (`content`) USING BTREE,
   INDEX `pinyin`(`pinyin` ASC) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of keywords
 -- ----------------------------
-INSERT INTO `keywords` VALUES (1, '中国知网', 'zhongguozhiwang');
-INSERT INTO `keywords` VALUES (2, '中国', 'zhongguo');
-INSERT INTO `keywords` VALUES (3, '中国银行', 'zhongguoyinhang');
-INSERT INTO `keywords` VALUES (4, '中国地图', 'zhongguoditu');
-INSERT INTO `keywords` VALUES (5, '中国裁判文书网', 'zhongguocaipanwenshuwang');
-INSERT INTO `keywords` VALUES (6, '中国奇谭', 'zhongguoqitan');
-INSERT INTO `keywords` VALUES (7, '中国电信', 'zhongguodianxin');
-INSERT INTO `keywords` VALUES (8, '中国人事考试网', 'zhongguorenshikaoshiwang');
-INSERT INTO `keywords` VALUES (9, '中国裁判文书公开网查询', 'zhongguocaipanwenshuwanggongkaichaxun');
-INSERT INTO `keywords` VALUES (10, '中国移动', 'zhongguoyidong');
-INSERT INTO `keywords` VALUES (11, '中国联通', 'zhongguoliantong');
-INSERT INTO `keywords` VALUES (12, '可以吃糖吗', 'keyichitangma');
+INSERT INTO `keywords` VALUES ('可以吃糖吗', 'keyichitangma');
+INSERT INTO `keywords` VALUES ('可以喝水吗', 'keyiheshuima');
+INSERT INTO `keywords` VALUES ('中国', 'zhongguo');
+INSERT INTO `keywords` VALUES ('中国裁判文书网', 'zhongguocaipanwenshuwang');
+INSERT INTO `keywords` VALUES ('中国裁判文书公开网查询', 'zhongguocaipanwenshuwanggongkaichaxun');
+INSERT INTO `keywords` VALUES ('中国电信', 'zhongguodianxin');
+INSERT INTO `keywords` VALUES ('中国地图', 'zhongguoditu');
+INSERT INTO `keywords` VALUES ('中国联通', 'zhongguoliantong');
+INSERT INTO `keywords` VALUES ('中国农业银行', 'zhongguonongyeyinhang');
+INSERT INTO `keywords` VALUES ('中国奇谭', 'zhongguoqitan');
+INSERT INTO `keywords` VALUES ('中国人事考试网', 'zhongguorenshikaoshiwang');
+INSERT INTO `keywords` VALUES ('中国移动', 'zhongguoyidong');
+INSERT INTO `keywords` VALUES ('中国银行', 'zhongguoyinhang');
+INSERT INTO `keywords` VALUES ('中国知网', 'zhongguozhiwang');
+INSERT INTO `keywords` VALUES ('中土', 'zhongtu');
+INSERT INTO `keywords` VALUES ('中途', 'zhongtu');
+INSERT INTO `keywords` VALUES ('种植', 'zhongzhi');
 
 SET FOREIGN_KEY_CHECKS = 1;
